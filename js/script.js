264244e605e0d180dc55202c485bf8f0da3d709a
Tofu <e.alvarado@meditrial.net>
2021-09-13 13:12:34 -0400

    Finished up minor javascript.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-const spaceShow = document.querySelectorAll('.method');
-const spaceBar = document.querySelector('bar');
+const methods = document.querySelectorAll('[data-tag]');
+const fullBar = document.querySelector('.bottom');
 
+let storageSpace = () => {
+    methods.forEach(method => {
+        method.addEventListener("click", () => {
+            if (method.classList.contains('storage')) {
+                method.classList.remove('storage');
+            } else {
+                const methodWithStorage = document.querySelectorAll(".storage");
+                methodWithStorage.forEach((methodWithStorage) => {
+                    methodWithStorage.classList.remove('storage');
+                })
+                method.classList.add('storage');
+            }
+})
+})}
+storageSpace();
